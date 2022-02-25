84cc16d4705da2b0f1f308fa8fe8e54820438bda
Richard Goulter <richard.goulter@gmail.com>
2022-02-25 16:51:09 +0700

    Pin nixpkgs to a version where moderncv works with fontawesome

--- default.nix
+++ default.nix
@@ -1,9 +1,15 @@
-{ nixpkgs ? import <nixpkgs> {}
+{ nixpkgs ? import (builtins.fetchGit {
+    name = "nixpkgs-with-texlive-with-moderncv";
+    url = "https://github.com/NixOS/nixpkgs/";
+    # Found by considering history of pkgs.nix:
+    # https://github.com/NixOS/nixpkgs/commits/master/pkgs/tools/typesetting/tex/texlive/pkgs.nix
+    rev = "c5787e5b8cc68d9478758b742f5da43b5ee7f909";
+  }) {}
   # Can change "template" (for "template.tex")
   # to "filename" (for "filename.tex");
   # then can run `nix-build` to generate the PDF
   # to "./result/filename.pdf".
-, document ? "template"
+, document ? "cv"
 }:
 with nixpkgs.pkgs;
 stdenv.mkDerivation {
